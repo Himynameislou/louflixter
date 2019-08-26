fd79eb68ccbf18979399767fb210ca9dd8875762
Himynameislou <louie1598@gmail.com>
2019-08-26 18:03:29 +0000

    Revert "L21, added video show page"
    This reverts commit bb8921732e0c1bd73bb9c4b2fa9e64d57ba253c4.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     devise_for :models
     root 'static_pages#index'
     resources :courses, only: [:index, :show]
-    resources :lessons, only: [:show]
     namespace :instructor do
       resources :sections, only: [] do
         resources :lessons, only: [:new, :create]
